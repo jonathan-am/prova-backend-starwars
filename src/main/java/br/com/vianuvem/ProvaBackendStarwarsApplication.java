17aac7c2207bb2900beb2ef522b3eea5aa04e0ad
Jonathan Alan de Moura <>
2021-05-13 13:32:58 -0300

    Project Starter

---
+++ src/main/java/br/com/vianuvem/ProvaBackendStarwarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.vianuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaBackendStarwarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaBackendStarwarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vianuvem/ProvaBackendStarwarsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vianuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaBackendStarwarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
